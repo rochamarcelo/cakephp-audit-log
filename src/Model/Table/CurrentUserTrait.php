0a702fa8272cda6c4b07f98f462dd741233091a5
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2017-07-21 11:38:45 +0200

    Add support for custom description read from session

--- src/Model/Table/CurrentUserTrait.php
+++ src/Model/Table/CurrentUserTrait.php
@@ -9,11 +9,19 @@ trait CurrentUserTrait
     {
         $request = Request::createFromGlobals();
         $session = $request->session();
+        $username = $session->read('Auth.User.username');
+
+        if ($session->check('Auditable.auditDescription')) {
+            $description = $session->consume('Auditable.auditDescription');
+        } else {
+            $description = h(sprintf('Action by %s', $username));
+        }
+
         return [
-            'id' => $session->read('Auth.User.username'),
+            'id' => $username,
             'ip' => $request->env('REMOTE_ADDR'),
             'url' => $request->here(),
-            'description' => $session->read('Auth.User.username'),
+            'description' => $description,
         ];
     }
 }
